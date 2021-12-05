57e9efa90e290dd339c0043d5ce0288127692b93
Hao Xu <74469369+HildaXu@users.noreply.github.com>
2021-12-05 11:59:35 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,26 +3,18 @@ import json
 from collections import Counter
 #import torch
 
+
+
+
+#===========================================#
+#              Streamlit Layout             #
+#===========================================#
 header = st.container()
-#dateset = st.container()
-#features = st.container()
-#model_Training = st.container()
 
 desc = "Check out the code [here](https://github.com/HildaXu/Beatles_Lyrics_Generator)!"
 with header:
 	st.title("Welcome to the Beatles Lyrics Generator!❤️✌️")
 	st.write(desc)
-	
-#with dateset:
-	#st.header('Dataset:')
-	#st.text('The dataset is derived from kaggle. https://www.kaggle.com/jenlooper/beatles-lyrics')
-
-#with features:
-	#st.header('The features:')
-
-#with model_Training:
-	#st.header('Time to train model!')
-	#st.text('Choose the number of words')
 
 num_sentences = st.number_input('Number of words', min_value=1, max_value=20, value=5)
 user_input = st.text_input('Seed Text (can leave blank)')
