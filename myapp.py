3ffdad9f9e3c30a53ecf0a5f97f540d45781c040
Hao Xu <74469369+HildaXu@users.noreply.github.com>
2021-12-05 12:14:01 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -17,9 +17,9 @@ with backgroud_image:
 	    .reportview-container {
 		background: url("https://media.vogue.fr/photos/5c4f0adebc36eb75cd6d6104/16:9/w_2112,h_1188,c_limit/91pNsAaJgxL.jpg")
 	    }
-	   .sidebar .sidebar-content {
-		background: url("https://media.vogue.fr/photos/5c4f0adebc36eb75cd6d6104/16:9/w_2112,h_1188,c_limit/91pNsAaJgxL.jpg")
-	    }
+	   #.sidebar .sidebar-content {
+	#	background: url("https://media.vogue.fr/photos/5c4f0adebc36eb75cd6d6104/16:9/w_2112,h_1188,c_limit/91pNsAaJgxL.jpg")
+	   # }
 	    </style>
 	    """,
 	    unsafe_allow_html=True
